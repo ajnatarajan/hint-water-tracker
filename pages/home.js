4f4dbf6a01c2cdff84864501a0a30c0e0035ad11
ajnatarajan <ajnatarajan@gmail.com>
2022-09-25 11:37:34 -0700

    [auth0] configured auth0 stuff so name and company are accessible

--- pages/home.js
+++ pages/home.js
@@ -4,11 +4,13 @@ import React from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 const Home = withPageAuthRequired(({ user }) => {
+  console.log(user);
+  console.log("OY");
   return (
     // Makes sure user is loaded before rendering anything
     user && (
       <div className={styles.heading}>
-        Hey there {user.name}
+        Hey there {user.name}, welcome to {user.company}
         <Button text="log out" href="/api/auth/logout" />
       </div>
     )
